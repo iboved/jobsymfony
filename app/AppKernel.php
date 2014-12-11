9e598c508554062a238f303bf4e8eceec28eacc3
Vladimir Gavrylov <iboved@gmail.com>
2014-12-11 11:56:50 +0200

    Added AdvertController

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
-            new AppBundle\AppBundle(),
             new Iboved\AdvertBundle\IbovedAdvertBundle(),
         );
 
---
+++ src/Iboved/AdvertBundle/Controller/AdvertController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Iboved\AdvertBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class AdvertController extends Controller
+{
+    /**
+     * @Template
+     * @Route("/", name="good")
+     * @Method({"GET"})
+     */
+    public function indexAction()
+    {
+        return array("hello" => "Hello, World!");
+    }
+}
